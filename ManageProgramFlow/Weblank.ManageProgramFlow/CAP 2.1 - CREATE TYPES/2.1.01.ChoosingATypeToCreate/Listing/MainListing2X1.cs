eaf3069c9a65734076153593f2e60c3f32424f82
Lucas Barbosa <contato@lucasbarbosa.com.br>
2018-09-25 21:44:54 -0300

    Add MainListing

---
+++ ManageProgramFlow/Weblank.ManageProgramFlow/CAP 2.1 - CREATE TYPES/2.1.01.ChoosingATypeToCreate/Listing/MainListing2X1.cs
@@ -0,0 +1,22 @@
+﻿using ChoosingATypeToCreate.Enumerators;
+using System;
+
+namespace ChoosingATypeToCreate.Listing
+{
+	public class MainListing2X1
+	{
+		// LISTING 2-1 Using the FlagAttribute for an enum
+		public MainListing2X1()
+		{
+			Console.WriteLine("----------------------------------------------------------------");
+			Console.WriteLine("MainListing2X1");
+			Console.WriteLine("----------------------------------------------------------------");
+
+			Days readingDays = Days.Monday | Days.Saturday;
+
+			Console.WriteLine(readingDays);
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ ManageProgramFlow/Weblank.ManageProgramFlow/CAP 2.1 - CREATE TYPES/2.1.01.ChoosingATypeToCreate/Listing/MainListing2X7.cs
@@ -0,0 +1,21 @@
+﻿using ChoosingATypeToCreate.Entities;
+using System;
+
+namespace ChoosingATypeToCreate.Listing
+{
+	public class MainListing2X7
+	{
+		// LISTING 2-7 Using named and optional arguments
+		public MainListing2X7()
+		{
+			Console.WriteLine("----------------------------------------------------------------");
+			Console.WriteLine("MainListing2X7");
+			Console.WriteLine("----------------------------------------------------------------");
+
+			Calculator C = new Calculator();
+			C.MyMethod(1, thirdArgument: true);
+
+			Console.ReadLine();
+		}
+	}
+}
